24802561d3a00797f28b842a2fbe2dddc4715e93
OpenShift guest <simone.busoli@gmail.com>
2013-10-04 19:21:33 -0400

    modified karma configuration file to run unit tests on cloud9

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,20 +1,27 @@
-basePath = '../';
+module.exports = function(config) {
+    config.set({
+        basePath: '../',
+        
+        hostname: process.env.IP || 'localhost',
+        port: process.env.PORT || 9876,
+        
+        frameworks: ['jasmine'],
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
-
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+        files : [
+          'app/lib/angular/angular.js',
+          'app/lib/angular/angular-*.js',
+          'test/lib/angular/angular-mocks.js',
+          'app/js/**/*.js',
+          'test/unit/**/*.js'
+        ],
+        
+        autoWatch: true,
+        
+        browsers: ['PhantomJS'],
+        
+        junitReporter: {
+          outputFile: 'test_out/unit.xml',
+          suite: 'unit'
+        },
+    });
 };
